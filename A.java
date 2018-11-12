a67caecb6594a126705a95752115f75a0ebe6059
Sudarsanam <Sudarsanam05@users.noreply.github.com>
2018-11-12 22:49:51 +0530

    Java
    Payilagam

---
+++ A.java
@@ -0,0 +1,25 @@
+package com.oops;
+
+public class A {
+
+	public int amt=100;
+	
+//	A obj=new A();
+	
+	void sum1()
+	{
+		int a=4;
+		System.out.println("Sum1" +a);
+	}
+	A()
+	{
+		System.out.println("Constructor");
+	}
+	class B
+	{
+		
+	}
+	
+	public final int pay=200;
+	
+}
---
+++ B.java
@@ -0,0 +1,17 @@
+package com.oops;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		A ob=new A();
+		ob.sum1();
+		int z=ob.amt;
+		System.out.println(z);
+		int sal=1000+z;
+		System.out.println(sal);
+		System.out.println(ob.amt);
+	}
+
+}
